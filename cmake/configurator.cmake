8909ce998eb6b7f54d4e447bd76ede329753a106
leo <leo.cluzel@idopte.com>
2025-11-25 09:33:11 +0100

    add O::Configurator

---
+++ cmake/configurator.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    OConfigurator
+    GIT_REPOSITORY https://github.com/leocity207/OConfigurator.git
+    GIT_TAG V0.0.1
+)
+
+FetchContent_MakeAvailable(OConfigurator)
